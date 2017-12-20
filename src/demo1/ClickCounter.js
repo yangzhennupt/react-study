2741c34f6e1930e1e976bfc7b7724b7054e89ae5
杨振 <yangzhennupt@163.com>
2017-12-20 15:41:01 +0800

    简单例子计数器

---
+++ src/demo1/ClickCounter.js
@@ -0,0 +1,24 @@
+import  React , {Component} from  'react';
+
+class ClickCounter extends Component{
+    constructor(props){
+        super(props);
+        this.onClickButton = this.onClickButton.bind(this);
+        this.state = {count:0}
+    }
+    onClickButton(){
+        this.setState({
+            count:this.state.count+1
+        });
+    }
+    render(){
+        return (
+            <div>
+                <button onClick = {this.onClickButton}>Click Me</button>
+                <p>Click Count:{this.state.count}</p>
+            </div>
+        )
+    }
+}
+
+export default ClickCounter;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import ClickCounter from './demo1/ClickCounter';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <ClickCounter />,
+    document.getElementById('root')
+);
 registerServiceWorker();
